439b4af8f9fd7e9ae9fb969359821d67e8b3875d
Lucio Andrés Illanes Albornoz <lucio@lucioillanes.de>
2018-11-29 13:36:20 +0100

    Python/AlAdhanAwqat.py: general cleanup.

--- Python/AlAdhanAwqat.py
+++ Python/AlAdhanAwqat.py
@@ -10,7 +10,21 @@ import hashlib, json, os, requests, sys, time, urllib.request
 
 class AlAdhanAwqat(object):
     """Fetch monthly awqāt from AlAdhan.com"""
+    # {{{ Class attributes
     apiUrlBase = "http://api.aladhan.com/v1/calendarByCity"
+    helpString = """usage: {argv0} [-h]
+       [-c city] [-C dname] [-f] [-F timing[,timing..]]
+       [-p list|tmux] [-t country] [-v] [-w offset]
+
+       -h....................: show this screen
+       -c city...............: specifies location city
+       -C dname..............: specifies cache directory pathname
+       -f....................: force (re)fetch from AlAdhan.com (defaults to: {self.optionsDefault[forceFetch]})
+       -F timing[,timing..]..: override default timings filter (defaults to: {self.optionsDefault[awqatFilter]})
+       -p list|tmux..........: specifies output format (defaults to {self.optionsDefault[outputFormat]})
+       -t country............: specifies location country
+       -v....................: increase verbosity (defaults to: {self.optionsDefault[verbose]})
+       -w offset.............: specifies waqt [-+] offset for -p tmux (defaults to: {self.optionsDefault[waqtOffset]})"""
     optionsDefault = {
         "awqatFilter":["Fajr", "Sunrise", "Dhuhr", "Asr", "Maghrib", "Isha"],
         "cachePathBase":os.path.expanduser(os.path.join("~", ".cache", "AlAdhanAwqat")),
@@ -20,7 +34,41 @@ class AlAdhanAwqat(object):
     optionsStringMap = {
         "c":"city", "C":"cachePathBase", "f":"forceFetch", "F":"awqatFilter",
         "h":"help", "p":"outputFormat", "t":"country", "v":"verbose", "w":"waqtOffset"}
-
+    # }}}
+    # {{{ _getData(self, apiUrlBase, cachePathBase, city, country, forceFetch): XXX
+    def _getData(self, apiUrlBase, cachePathBase, city, country, forceFetch):
+        cacheKey = "{}\0{}\0{}\0{}\0".format(apiUrlBase, city, country, time.strftime("%d%m%Y"))
+        cacheFileName = hashlib.sha256(cacheKey.encode()).hexdigest()
+        cacheFilePathName = os.path.join(cachePathBase, cacheFileName)
+        rc = False
+        if not forceFetch:
+            rc, status, data = self._getDataCache(apiUrlBase, cacheFilePathName, city, country)
+        if not rc:
+            rc, status, data = self._getDataFetch(apiUrlBase, cacheFilePathName, city, country)
+        return rc, status, data
+    # }}}
+    # {{{ _getDataCache(self, apiUrlBase, cacheFilePathName, city, country): XXX
+    def _getDataCache(self, apiUrlBase, cacheFilePathName, city, country):
+        if not os.path.isdir(os.path.dirname(cacheFilePathName)):
+            os.makedirs(os.path.dirname(cacheFilePathName))
+        if not os.path.exists(cacheFilePathName):
+            return False, None, None
+        else:
+            with open(cacheFilePathName, "r") as fileObject:
+                data = json.load(fileObject)
+            return True, "OK", data
+    # }}}
+    # {{{ _getDataFetch(self, apiUrlBase, cacheFilePathName, city, country): XXX
+    def _getDataFetch(self, apiUrlBase, cacheFilePathName, city, country):
+        apiUrl = "{}?city={}&country={}".format(apiUrlBase, city, country)
+        self._log("Fetching [4m{apiUrl}[0m...".format(**locals()), isVerbose=True)
+        data, rc = requests.get(apiUrl).json(), False
+        if data["status"] == "OK":
+            with open(cacheFilePathName, "w+") as fileObject:
+                json.dump(data["data"], fileObject)
+            rc = True
+        return rc, data["status"], data["data"]
+    # }}}
     # {{{ _log(self, msg): Log single message to std{err,out} w/ timestamp
     def _log(self, msg, isError=False, isVerbose=False):
         if isError:
@@ -29,48 +77,6 @@ class AlAdhanAwqat(object):
             print("{} [36m{}[0m".format(time.strftime("%d-%^b-%Y %H:%M:%S"), msg))
         elif not isVerbose:
             print("{} {}".format(time.strftime("%d-%^b-%Y %H:%M:%S"), msg))
-    # }}}
-    # {{{ _usage(self, argv0, options): XXX
-    def _usage(self, argv0, options):
-        if options["city"] == None:
-            print("error: missing city", file=sys.stderr)
-        if options["country"] == None:
-            print("error: missing country", file=sys.stderr)
-        print("usage: {} [-h]".format(argv0), file=sys.stderr)
-        print("       [-c city] [-C dname] [-f] [-F timing[,timing..]]".format(argv0), file=sys.stderr)
-        print("       [-p list|tmux] [-t country] [-v] [-w offset]".format(argv0), file=sys.stderr)
-        print("", file=sys.stderr)
-        print("       -h....................: show this screen", file=sys.stderr)
-        print("       -c city...............: specifies location city", file=sys.stderr)
-        print("       -C dname..............: specifies cache directory pathname", file=sys.stderr)
-        print("       -f....................: force (re)fetch from AlAdhan.com (defaults to: {})".format(self.optionsDefault["forceFetch"]), file=sys.stderr)
-        print("       -F timing[,timing..]..: override default timings filter (defaults to: {})".format(", ".join(self.optionsDefault["awqatFilter"])), file=sys.stderr)
-        print("       -p list|tmux..........: specifies output format (defaults to {})".format(self.optionsDefault["outputFormat"]), file=sys.stderr)
-        print("       -t country............: specifies location country ", file=sys.stderr)
-        print("       -v....................: increase verbosity (defaults to: {})".format(self.optionsDefault["verbose"]), file=sys.stderr)
-        print("       -w offset.............: specifies waqt [-+] offset for -p tmux (defaults to: {})".format(self.optionsDefault["waqtOffset"]), file=sys.stderr)
-    # }}}
-    # {{{ _getData(self, apiUrlBase, cachePathBase, city, country, forceFetch): XXX
-    def _getData(self, apiUrlBase, cachePathBase, city, country, forceFetch):
-        cacheKey = "{}\0{}\0{}\0{}\0".format(apiUrlBase, city, country, time.strftime("%d%m%Y"))
-        cacheFileName = hashlib.sha256(cacheKey.encode()).hexdigest()
-        if not os.path.isdir(cachePathBase):
-            os.makedirs(cachePathBase)
-        cacheFilePathName = os.path.join(cachePathBase, cacheFileName)
-        if not os.path.exists(cacheFilePathName) or forceFetch:
-            apiUrl = "{}?city={}&country={}".format(apiUrlBase, city, country)
-            self._log("Fetching [4m{apiUrl}[0m...".format(**locals()), isVerbose=True)
-            data = requests.get(apiUrl).json()
-            if data["status"] == "OK":
-                with open(cacheFilePathName, "w+") as fileObject:
-                    json.dump(data["data"], fileObject)
-                return True, data["status"], data["data"]
-            else:
-                return False, "OK", data["data"]
-        else:
-            with open(cacheFilePathName, "r") as fileObject:
-                data = json.load(fileObject)
-            return True, "OK", data
     # }}}
     # {{{ _printAsList(self, timings): XXX
     def _printAsList(self, timings):
@@ -99,6 +105,14 @@ class AlAdhanAwqat(object):
             else:
                 timingsList += ["#[fg=green]" + timingValue + "#[fg=default]"]
         print(" ".join(timingsList))
+    # }}}
+    # {{{ _usage(self, argv0, options): XXX
+    def _usage(self, argv0, options):
+        if options["city"] == None:
+            print("error: missing city", file=sys.stderr)
+        if options["country"] == None:
+            print("error: missing country", file=sys.stderr)
+        print(self.helpString.format(**locals()))
     # }}}
     # {{{ synchronise(self): XXX
     def synchronise(self):
