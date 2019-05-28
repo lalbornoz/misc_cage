586e7a402d9acfbbfad086bf3db0715bc252ec19
Lucio Andrés Illanes Albornoz <lucio@lucioillanes.de>
2019-05-28 11:45:33 +0200

    Shell/{cba-updates.sh,provision.ps1}: updated.

--- Shell/cba-updates.sh
+++ Shell/cba-updates.sh
@@ -1,10 +1,56 @@
 #!/bin/sh
 
 # {{{ Default variables
+DEFAULT_COLOUR_DIST_UPGRADE="94";
 DEFAULT_COLOUR_FAILURE="91";
+DEFAULT_COLOUR_RDEPENDS="96";
 DEFAULT_COLOUR_SUCCESS="32";
 DEFAULT_COLOUR_TIMESTAMP="33";
 DEFAULT_USER="toor";
+# }}}
+# {{{ Remote script variable
+REMOTE_SCRIPT='
+	fini() { local _log_fname="${1}"; echo "${rc_last:-0} fini"; cat "${_log_fname}"; rm -f "${_log_fname}"; };
+	init() { dpkg_new_fnames=""; pkgs=""; pkgs_rdepends=""; rc=""; rc_last=""; log_fname="$(mktemp)" || exit 1; };
+	status() { local _rc="${1}"; echo "${*}"; rc_last="${_rc}"; if [ "${_rc}" -ne 0 ]; then exit "${_rc}"; fi; };
+	init; trap "fini \"${log_fname}\"" EXIT HUP INT QUIT TERM USR1 USR2;
+
+	# apt-get -y update
+	apt-get -y update >>"${log_fname}" 2>&1;
+	status "${?}" update;
+
+	# apt-get -y dist-upgrade
+	pkgs="$(apt-get -y dist-upgrade 2>&1)"; rc="${?}";
+	printf "%s\n" "${pkgs}" >>"${log_fname}";
+	pkgs="$(printf "%s\n" "${pkgs}"									|
+		awk '\''
+			$0 == "The following packages will be upgraded:" {m=1; next}
+			m {if ($0 !~ /^  /) {m=0} else {print}}'\'')";
+	pkgs="$(printf "%s\n" "${pkgs}"									|
+		sed -ne "s/  */\n/gp" | sed -ne "/^ *$/d" -e "p" | paste -sd " ")";
+	status "${rc}" dist-upgrade "${pkgs}";
+
+	# apt-get -y autoremove --purge
+	apt-get -y autoremove --purge >>"${log_fname}" 2>&1;
+	status "${?}" autoremove;
+
+	# rm -f /var/cache/apt/archives/*.deb
+	rm -f /var/cache/apt/archives/*.deb >>"${log_fname}" 2>&1;
+	status "${?}" clean;
+
+	if [ -n "${pkgs}" ]; then
+		# apt-cache rdepends --installed
+		pkgs_rdepends="$(apt-cache rdepends --installed ${pkgs} 2>&1)"; rc="${?}";
+		printf "%s\n" "${pkgs_rdepends}" >>"${log_fname}";
+		pkgs_rdepends="$(printf "%s\n" "${pkgs_rdepends}"					|
+			sed -n -e "s/^\s\+|\?//" -e "/^Reverse Depends:\$/d" -e "/^lib/d" -e "p"	|
+			sort | uniq | paste -sd " ")";
+		status "${rc}" rdepends "${pkgs_rdepends}";
+
+		# find /etc -name *.dpkg-new
+		dpkg_new_fnames="$(find /etc -name *.dpkg-new 2>/dev/null)";
+		status "${?}" dpkg-new "${dpkg_new_fnames}";
+	fi';
 # }}}
 # {{{ Private subroutines
 logf() {
@@ -13,71 +59,35 @@ logf() {
 };
 
 printf_rc() {
-	local _rc="${1}" _fmt="${2}"; shift 2;
+	local _colour="${1:-${DEFAULT_COLOUR_SUCCESS}}" _rc="${2}" _fmt="${3}"; shift 3;
 	if [ "${_rc}" -eq 0 ]; then
-		printf "[${DEFAULT_COLOUR_SUCCESS}m${_fmt}[0m" "${@}";
+		printf "[${_colour}m${_fmt}[0m" "${@}";
 	else
 		printf "[${DEFAULT_COLOUR_FAILURE}m${_fmt}[0m" "${@}";
 	fi;
 };
 # }}}
-
 update_host() {
 	local _host="${1}" _lflag="${2}" _user="${3}" _log_data="" _log_fname="";
 
 	_log_fname="${0##*/}.${_host%%.}.log";
 	logf "%s:" "${_host}";
-	ssh -l"${_user}" -T "${_host}" '
-		fini() { local _log_fname="${1}"; echo "${rc_last:-0} fini"; cat "${_log_fname}"; rm -f "${_log_fname}"; };
-		init() { dpkg_new_fnames=""; pkgs=""; pkgs_filtered=""; pkgs_rdepends=""; rc=""; rc_last=""; log_fname="$(mktemp)" || exit 1; };
-		status() { local _rc="${1}"; echo "${*}"; rc_last="${_rc}"; if [ "${_rc}" -ne 0 ]; then exit "${_rc}"; fi; };
-		init; trap "fini \"${log_fname}\"" EXIT HUP INT QUIT TERM USR1 USR2;
-
-		# apt-get -y update
-		apt-get -y update >>"${log_fname}" 2>&1;
-		status "${?}" update;
-
-		# apt-get -y dist-upgrade
-		pkgs="$(apt-get -y dist-upgrade 2>&1)"; rc="${?}";
-		printf "%s\n" "${pkgs}" >>"${log_fname}";
-		pkgs="$(printf "%s\n" "${pkgs}"								|
-			awk '\''
-				$0 == "The following packages will be upgraded:" {m=1; next}
-				m {if ($0 !~ /^  /) {m=0} else {print}}'\'')";
-		pkgs_filtered="$(printf "%s\n" "${pkgs}"						|
-			sed -ne "s/  */\n/gp" | sed -ne "/^ *$/d" -e "/^lib/d" -e "p" | paste -sd " ")";
-		pkgs="$(printf "%s\n" "${pkgs}"						|
-			sed -ne "s/  */\n/gp" | sed -ne "/^ *$/d" -e "p" | paste -sd " ")";
-		status "${rc}" dist-upgrade "${pkgs_filtered}";
-
-		# apt-get -y autoremove --purge
-		apt-get -y autoremove --purge >>"${log_fname}" 2>&1;
-		status "${?}" autoremove;
-
-		# rm -f /var/cache/apt/archives/*.deb
-		rm -f /var/cache/apt/archives/*.deb >>"${log_fname}" 2>&1;
-		status "${?}" clean;
-
-		if [ -n "${pkgs}" ]; then
-			# apt-cache rdepends --installed
-			pkgs_rdepends="$(apt-cache rdepends --installed ${pkgs} 2>&1)"; rc="${?}";
-			printf "%s\n" "${pkgs_rdepends}" >>"${log_fname}";
-			pkgs_rdepends="$(printf "%s\n" "${pkgs_rdepends}"				|
-				sed -ne "/^Reverse Depends:\$/d" -e "/^lib/d" -e "s/^\s\+|\?//" -e "p"	|
-				sort | uniq | paste -sd " ")";
-			status "${rc}" rdepends "${pkgs_rdepends}";
-
-			# find /etc -name *.dpkg-new
-			dpkg_new_fnames="$(find /etc -name *.dpkg-new 2>/dev/null)";
-			status "${?}" dpkg-new "${dpkg_new_fnames}";
-		fi' 2>/dev/null |\
+	ssh -l"${_user}" -T "${_host}" "${REMOTE_SCRIPT}" 2>/dev/null |\
 	while read -r _rc _type _msg; do
 	case "${_type}" in
-	autoremove|clean|update)
-			printf_rc "${_rc}" " %s" "${_type}"; ;;
-	dist-upgrade|dpkg-new|rdepends)
-			printf_rc "${_rc}" " %s(%s)" "${_type}" "${_msg}"; ;;
-	fini)		printf_rc "${_rc}" " %s" "[fetching log]";
+	autoremove)
+			printf_rc "" "${_rc}" " %s" "${_type}"; ;;
+	clean)
+			printf_rc "" "${_rc}" " %s" "${_type}"; ;;
+	dist-upgrade)
+			printf_rc "${DEFAULT_COLOUR_DIST_UPGRADE}" "${_rc}" " %s(%s)" "${_type}" "${_msg}"; ;;
+	dpkg-new)
+			printf_rc "" "${_rc}" " %s(%s)" "${_type}" "${_msg}"; ;;
+	rdepends)
+			printf_rc "${DEFAULT_COLOUR_RDEPENDS}" "${_rc}" " %s(%s)" "${_type}" "${_msg}"; ;;
+	update)
+			printf_rc "" "${_rc}" " %s" "${_type}"; ;;
+	fini)		printf_rc "" "${_rc}" " %s" "[fetching log]";
 			if [ "${_lflag:-0}" -eq 0 ]; then
 				_log_fname="/dev/null";
 			else
