b6075db7720d886527721977cae4a567288aa6e0
Lucio Andrés Illanes Albornoz <lucio@lucioillanes.de>
2019-06-03 12:24:53 +0200

    Shell/cba-updates.sh: updated.

--- Shell/cba-updates.sh
+++ Shell/cba-updates.sh
@@ -50,7 +50,7 @@ REMOTE_SCRIPT='
 
 		# dpkg -l [ ... ] | grep -Eq "^(/etc/init.d|/lib/systemd/system)/"
 		for pkg in ${pkg_rdepends}; do
-			if dpkg -l "${pkg}" 2>>"${log_fname}"						|
+			if dpkg -L "${pkg}" 2>>"${log_fname}"						|
 			   grep -Eq "^(/etc/init.d|/lib/systemd/system)/"; then
 				pkgs_rdepends_services="${pkgs_rdepends_services:+${pkgs_rdepends_services} ${pkg}}";
 			fi;
