badf7025c9aa4d48e89301a040629ee49d827d34
Lucio Andrés Illanes Albornoz <lucio@lucioillanes.de>
2020-07-02 09:16:39 +0200

    Python/apt-upgrades.py: updated.

--- Python/apt-upgrades.py
+++ Python/apt-upgrades.py
@@ -229,10 +229,9 @@ The reverse dependencies, sans library packages, listed above encompass the foll
         parser.add_argument("-v", "--verbose", action="store_true", default=False, dest="verbose")
         self.args = parser.parse_args()
         self.logger = AptUpgradesLogger(initialLevel=AptUpgradesLogger.VERBOSE if self.args.verbose else logging.INFO)
-        self.cacheNewFilename = os.path.abspath(os.path.expanduser(self.cacheNewFileName))
-        if self.args.new:
-            if not os.path.exists(os.path.dirname(self.cacheNewFileName)):
-                os.makedirs(os.path.dirname(self.cacheNewFileName))
+        self.cacheNewFileName = os.path.abspath(os.path.expanduser(self.cacheNewFileName))
+        if not os.path.exists(os.path.dirname(self.cacheNewFileName)):
+            os.makedirs(os.path.dirname(self.cacheNewFileName))
 
 if __name__ == "__main__":
     exit(AptUpgrades().main())
