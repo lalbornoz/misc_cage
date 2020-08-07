9d9320635fec967f30b7026484994b652a749d3b
Lucio Andrés Illanes Albornoz <lucio@lucioillanes.de>
2020-08-07 13:54:25 +0200

    Shell/cba-updates.sh: updated.

--- Shell/cba-updates.sh
+++ Shell/cba-updates.sh
@@ -92,7 +92,7 @@ update_host() {
 	||   ! mkfifo "${_rc_fifo_fname}"; then
 		printf_rc "" 1 " [failed to (re)create FIFO \`%s'.]\n" "${_rc_fifo_fname}";
 	else	trap "rm -f \"${_rc_fifo_fname}\" >/dev/null 2>&1" EXIT HUP INT TERM USR1 USR2;
-		{ set +o errexit; exec 3>"${_rc_fifo_fname}"; ssh -l"${_user}" -T "${_host}" "${REMOTE_SCRIPT}" 2>/dev/null; echo "${?}" >&3; } | {
+		{ sleep 1; set +o errexit; exec 3>"${_rc_fifo_fname}"; ssh -l"${_user}" -T "${_host}" "${REMOTE_SCRIPT}" 2>/dev/null; echo "${?}" >&3; } | {
 		exec 3<>"${_rc_fifo_fname}";
 		while true; do
 			if [ "${_rc_fifo_fl:-0}" -eq 0 ]; then
